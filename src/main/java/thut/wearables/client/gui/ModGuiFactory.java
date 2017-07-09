9f27d41fa8d2af08c9719ed10f78b2d20ad4a5fc
Thutmose <elpatricimo@gmail.com>
2017-07-09 15:55:32 -0400

    remove deprecated and removed methods.

--- src/main/java/thut/wearables/client/gui/ModGuiFactory.java
+++ src/main/java/thut/wearables/client/gui/ModGuiFactory.java
@@ -5,43 +5,29 @@ import java.util.Set;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiScreen;
 import net.minecraftforge.fml.client.IModGuiFactory;
-import net.minecraftforge.fml.client.IModGuiFactory.RuntimeOptionCategoryElement;
-import net.minecraftforge.fml.client.IModGuiFactory.RuntimeOptionGuiHandler;
 
 public class ModGuiFactory implements IModGuiFactory
 {
-    @SuppressWarnings("deprecation")
-   // @Override
-    public RuntimeOptionGuiHandler getHandlerFor(RuntimeOptionCategoryElement element)
-    {
-        return null;
-    }
 
-  //  @Override
+    @Override
     public void initialize(Minecraft minecraftInstance)
     {
 
     }
 
-   // @Override
-    public Class<? extends GuiScreen> mainConfigGuiClass()
-    {
-        return ModGuiConfig.class;
-    }
-
-  //  @Override
+    @Override
     public Set<RuntimeOptionCategoryElement> runtimeGuiCategories()
     {
         return null;
     }
 
-   // @Override
+    @Override
     public boolean hasConfigGui()
     {
         return true;
     }
 
-   // @Override
+    @Override
     public GuiScreen createConfigGui(GuiScreen parentScreen)
     {
         return new ModGuiConfig(parentScreen);
