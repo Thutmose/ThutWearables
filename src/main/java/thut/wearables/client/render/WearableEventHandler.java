fec0ec97073cf9361a4e57761456d68c10ee95be
Thutmose <elpatricimo@gmail.com>
2019-05-04 00:01:37 -0400

    Adds tooltips for hotkeys
    Adds tooltips which say which hotkey activates the given slot.
    
    This addesses #8

--- src/main/java/thut/wearables/client/render/WearableEventHandler.java
+++ src/main/java/thut/wearables/client/render/WearableEventHandler.java
@@ -1,22 +1,29 @@
 package thut.wearables.client.render;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 import org.lwjgl.input.Keyboard;
 
+import com.google.common.collect.Maps;
 import com.google.common.collect.Sets;
 
 import net.minecraft.client.model.ModelBiped;
 import net.minecraft.client.renderer.entity.RenderLivingBase;
 import net.minecraft.client.renderer.entity.layers.LayerRenderer;
+import net.minecraft.client.resources.I18n;
 import net.minecraft.client.settings.KeyBinding;
 import net.minecraftforge.client.event.RenderLivingEvent;
+import net.minecraftforge.client.settings.KeyConflictContext;
+import net.minecraftforge.client.settings.KeyModifier;
+import net.minecraftforge.event.entity.player.ItemTooltipEvent;
 import net.minecraftforge.fml.client.registry.ClientRegistry;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.InputEvent.KeyInputEvent;
 import net.minecraftforge.fml.relauncher.ReflectionHelper;
 import thut.wearables.EnumWearable;
+import thut.wearables.IActiveWearable;
 import thut.wearables.ThutWearables;
 import thut.wearables.network.PacketGui;
 
@@ -30,6 +37,12 @@ public class WearableEventHandler
     {
         toggleGui = new KeyBinding("Toggle Wearables Gui", Keyboard.KEY_NONE, "Wearables");
         ClientRegistry.registerKeyBinding(toggleGui);
+
+        Map<Integer, Integer> defaults = Maps.newHashMap();
+        defaults.put(7, Keyboard.KEY_E);
+        defaults.put(2, Keyboard.KEY_Z);
+        defaults.put(3, Keyboard.KEY_X);
+
         for (int i = 0; i < 13; i++)
         {
             EnumWearable slot = EnumWearable.getWearable(i);
@@ -43,8 +56,10 @@ public class WearableEventHandler
             {
                 name = name + " " + slot + " " + subIndex;
             }
-            int key = slot == EnumWearable.BACK ? Keyboard.KEY_B : Keyboard.KEY_NONE;
-            keys[i] = new KeyBinding(name, key, "Wearables");
+            int key = defaults.containsKey(slot) ? defaults.get(slot) : Keyboard.KEY_NONE;
+            keys[i] = defaults.containsKey(slot) ? new KeyBinding(name, key, "Wearables")
+                    : new KeyBinding(name, KeyConflictContext.UNIVERSAL, KeyModifier.ALT, key, "Wearables");
+
             ClientRegistry.registerKeyBinding(keys[i]);
         }
     }
@@ -66,6 +81,32 @@ public class WearableEventHandler
         {
             PacketGui packet = new PacketGui();
             ThutWearables.packetPipeline.sendToServer(packet);
+        }
+    }
+
+    @SubscribeEvent
+    public void onToolTip(ItemTooltipEvent evt)
+    {
+        if (evt.getItemStack().hasCapability(IActiveWearable.WEARABLE_CAP, null))
+        {
+            IActiveWearable wear = evt.getItemStack().getCapability(IActiveWearable.WEARABLE_CAP, null);
+            EnumWearable slot = wear.getSlot(evt.getItemStack());
+            String key = keys[slot.index].getDisplayName();
+            String message = "";
+            switch (slot.slots)
+            {
+            case 2:
+                message = I18n.format("wearables.keyuse.left", key);
+                evt.getToolTip().add(message);
+                key = keys[slot.index + 1].getDisplayName();
+                message = I18n.format("wearables.keyuse.right", key);
+                evt.getToolTip().add(message);
+                break;
+            default:
+                message = I18n.format("wearables.keyuse.single", key);
+                evt.getToolTip().add(message);
+                break;
+            }
         }
     }
 
